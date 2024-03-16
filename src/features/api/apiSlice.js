4bad0310797175a6965d242c2c2fd2f5dd466fc2
Igor <qwerty2244668800@gmail.com>
2024-03-16 22:06:29 +0200

    changes for vercel deployment

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: "api",
     // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001" }),
-    baseQuery: fetchBaseQuery({ baseUrl: "https://pedrotech-davegray-mern.onrender.com" }),
+    // baseQuery: fetchBaseQuery({ baseUrl: "https://pedrotech-davegray-mern.onrender.com" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://pedrotech-davegray-mern-backend.vercel.app" }),
     tagTypes: ["Products", "Users"],
     endpoints: (builder) => ({}),
 });
